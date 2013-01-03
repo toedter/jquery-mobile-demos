4ee4370ed8d47fc9b6ea476838ef490ddabd7d83
Kai Toedter <kai.toedter@siemens.com>
2013-01-03 12:56:43 +0100

    minor enhancements

--- yelp/yelp.js
+++ yelp/yelp.js
@@ -1,39 +1,55 @@
 (function(window) {
 
-    var yelp = this, yelpKey = 'BzkG0eKlTgxErtaJyIxQCA'
+    var yelp = this, yelpKey = 'BzkG0eKlTgxErtaJyIxQCA';
 
-    yelp.test = function() {
-	console.log("test");
+    yelp.log = function(msg) {
+	console.log(msg);
     };
 
-    yelp.callYelp = function(lat, long) {
-	$.ajax({
-	    type : 'GET',
-	    Accept : "application/json",
-	    url : 'http://api.yelp.com/business_review_search?ywsid=' + yelpKey
-		    + '&term=Bars&lat=' + lat + '&long=' + long,
-	    crossDomain : true,
-	    dataType : 'jsonp',
+    yelp.callLatLong = function(lat, long) {
+	var params = '&lat=' + lat + '&long=' + long;
+	callWithParams(params);
+    };
 
-	    error : function(msg) {
-		console.log('got error: ' + msg.statusText);
-	    },
-	    success : function(msg) {
-		if (msg.message.text == 'OK') {
-		    var list = "";
-		    $.each(msg.businesses, function(i, item) {
-			list += '<li><a href="' + item.url + '">';
-			list += '<img src="' + item.photo_url + '" />';
-			list += '<img src="' + item.rating_img_url + '" />';
-			list += "<h3>" +  item.name + "</h3>";
-			list += "<p>" +  item.address1 +", " + item.city + "</p>";
-			list += '</a></li>';
-		    });
-		    $("ul:jqmData(role='listview')").append(list).listview("refresh");
-		}
-	    }
-	});
+    yelp.callAddress = function(address) {
+	var params = '&location=' + address;
+	callWithParams(params);
+    };
+
+    callWithParams = function(params) {
+	$("ul:jqmData(role='listview')").empty();
+	$.mobile.loading('show');
+	$
+		.ajax({
+		    type : 'GET',
+		    url : 'http://api.yelp.com/business_review_search?ywsid=' + yelpKey + '&term=Bars' + params
+			    + '&radius=2.0',
+		    crossDomain : true,
+		    dataType : 'jsonp',
+
+		    error : function(msg) {
+			console.log('got error: ' + msg.statusText);
+			$.mobile.loading('hide');
+		    },
+		    success : function(msg) {
+			$.mobile.loading('hide');
+
+			// console.log(msg.message);
+			if (msg.message.text == 'OK') {
+			    var list = "";
+			    $.each(msg.businesses, function(i, item) {
+				list += '<li><a href="' + item.url + '">';
+				list += '<img src="' + item.photo_url + '" />';
+				list += '<img src="' + item.rating_img_url + '" />';
+				list += "<h3>" + item.name + "</h3>";
+				list += "<p>" + item.address1 + ", " + item.city + "</p>";
+				list += '</a></li>';
+			    });
+			    $("ul:jqmData(role='listview')").append(list).listview("refresh");
+			}
+		    }
+		});
     }
 
-    window.yelp = this;
+    window.yelp = yelp;
 }(window));
