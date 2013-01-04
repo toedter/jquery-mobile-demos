b365bcfe068fdd4c305ff13ca4a413e41370c0a4
Kai Toedter <kai.toedter@siemens.com>
2013-01-04 14:24:57 +0100

    better initial page

--- yelp/yelp.js
+++ yelp/yelp.js
@@ -6,10 +6,12 @@
 	console.log(msg);
     };
 
+    $(document).on('pageinit', '#main', function() {
+	yelp.callLatLong('OOP',48.136848, 11.695118);
+    });
+
     $(document).on('pageinit', '#new', function() {
-
 	$('form').submit(function() {
-
 	    label = $('[name=label]').val();
 	    address = $('[name=address]').val();
 	    radius = $('[name=radius]').val();
@@ -18,7 +20,6 @@
 	    yelp.callAddress(label, address, radius);
 	    return false;
 	});
-
     });
 
     yelp.callLatLong = function(label, lat, long, radius) {
@@ -32,42 +33,41 @@
     };
 
     callWithParams = function(label, params, radius) {
-	if(!radius)
+	if (!radius)
 	    radius = 2;
-	
-	$('.ui-title').text(label);
+
+	$('.ui-title').text('Bars close to ' + label + ' (' + radius + ' km)');
 	$("ul:jqmData(role='listview')").empty();
 	$.mobile.loading('show');
-	$
-		.ajax({
-		    type : 'GET',
-		    url : 'http://api.yelp.com/business_review_search?ywsid=' + yelpKey + '&term=Bars' + params
-			    + '&radius=' + radius,
-		    crossDomain : true,
-		    dataType : 'jsonp',
+	$.ajax({
+	    type : 'GET',
+	    url : 'http://api.yelp.com/business_review_search?ywsid=' + yelpKey + '&term=Bars' + params + '&radius='
+		    + radius,
+	    crossDomain : true,
+	    dataType : 'jsonp',
 
-		    error : function(msg) {
-			console.log('got error: ' + msg.statusText);
-			$.mobile.loading('hide');
-		    },
-		    success : function(msg) {
-			$.mobile.loading('hide');
+	    error : function(msg) {
+		console.log('got error: ' + msg.statusText);
+		$.mobile.loading('hide');
+	    },
+	    success : function(msg) {
+		$.mobile.loading('hide');
 
-			// console.log(msg.message);
-			if (msg.message.text == 'OK') {
-			    var list = "";
-			    $.each(msg.businesses, function(i, item) {
-				list += '<li><a href="' + item.url + '">';
-				list += '<img src="' + item.photo_url + '" />';
-				list += '<img src="' + item.rating_img_url + '" />';
-				list += "<h3>" + item.name + "</h3>";
-				list += "<p>" + item.address1 + ", " + item.city + "</p>";
-				list += '</a></li>';
-			    });
-			    $("ul:jqmData(role='listview')").append(list).listview("refresh");
-			}
-		    }
-		});
+		// console.log(msg.message);
+		if (msg.message.text == 'OK') {
+		    var list = "";
+		    $.each(msg.businesses, function(i, item) {
+			list += '<li><a href="' + item.url + '">';
+			list += '<img src="' + item.photo_url + '" />';
+			list += '<img src="' + item.rating_img_url + '" />';
+			list += "<h3>" + item.name + "</h3>";
+			list += "<p>" + item.address1 + ", " + item.city + "</p>";
+			list += '</a></li>';
+		    });
+		    $("ul:jqmData(role='listview')").append(list).listview("refresh");
+		}
+	    }
+	});
     }
 
     window.yelp = yelp;
